6ac28147cea34fc06725545860855a63e48c538f
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-07-09 04:33:10 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,13 +1,14 @@
 require 'pry'
 
 class String
-
+#returns true if ending in a .
   def sentence?
-    
+    self.end_with?(".")
   end
 
+#return true if a question
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
